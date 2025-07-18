0c07cb07793a788dc83fddfb0582498dd24dc014
RONY-CHRISTIAN <151699019+RONY-CHRISTIAN@users.noreply.github.com>
2025-07-17 21:38:33 -0500

    Update scripta.js

--- scripta.js
+++ scripta.js
@@ -1,11 +1,12 @@
 // Texto que se escribe poco a poco con salto de línea automático
 const mensaje = [
-"Desde el primer momento en que nuestras palabras se encontraron, sentí una conexión única y especial. Cada conversación que hemos compartido ha revelado lo extraordinaria que eres. Mi corazón siente como si te conociera de toda la vida.     -Andres"];
+"Desde el primer momento en que nuestras palabras se encontraron, sentí una conexión única y especial. Cada conversación que hemos compartido ha revelado lo extraordinaria que eres. Mi corazón siente como si te conociera de toda la vida.-Andres"
+"-Andres"];
 
 let index = 0;
 let subIndex = 0;
 let lineaActual = 0;
-const velocidadEscritura = 100;
+const velocidadEscritura = 1; //100
 
 function escribirTexto() {
     if (lineaActual < mensaje.length) {
